db557306fea4d00771a53803f02ee70a3038c7e6
Emanuelvss13 <emanuelsouza2002@gmail.com>
2021-09-17 15:53:34 -0300

    feature: post preview

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,6 +2,7 @@ import { signIn, useSession } from 'next-auth/client'
 import { api } from '../../services/api'
 import styles from './style.module.scss'
 import { getStripeJs } from '../../services/stripe-js'
+import { useRouter } from 'next/dist/client/router'
 
 interface ButtonProps{
     priceId: string
@@ -9,10 +10,16 @@ interface ButtonProps{
 
 export default function SubscribeButton({priceId}: ButtonProps) {
     const [session] = useSession()
-    
+    const router = useRouter()
+
     async function handleSubscribe(){
         if(!session){
             signIn('github')
+            return
+        }
+
+        if(session.activeSubscription){
+            router.push('/posts')
             return
         }
 
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -36,6 +36,17 @@ export default function Post({post}: PostProps) {
 export const getServerSideProps: GetServerSideProps = async({req, params}) => {
   const session = await getSession({req})
 
+  console.log(session)
+
+  if(!session?.activeSubscription){
+    return{
+      redirect:{
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   const {slug} = params
 
   const prismic = getPrismicClient(req)
